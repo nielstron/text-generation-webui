69f3d7cc722014abe56cb22f97c9083f1e927467
Niels Mündler <n.muendler@posteo.de>
2023-04-04 11:29:30 +0200

    Finalize OpenAI-ish api

--- extensions/chat_api/script.py
+++ extensions/chat_api/script.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from threading import Thread
 
@@ -21,7 +22,8 @@ class Handler(BaseHTTPRequestHandler):
                         {
                             "id": shared.model_name,
                         }
-                    ]
+                    ],
+                    "object": "list",
                 }
             )
 
@@ -33,13 +35,32 @@ class Handler(BaseHTTPRequestHandler):
         content_length = int(self.headers["Content-Length"])
         body = json.loads(self.rfile.read(content_length).decode("utf-8"))
 
-        if self.path == "/api/v1/generate":
+        if self.path == "/api/v1/chat/completions":
+            model = body["model"]
+            if model != shared.model_name:
+                self.send_error(404)
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
 
-            prompt = body["prompt"]
-            prompt_lines = [l.strip() for l in prompt.split("\n")]
+            messages = body["messages"]
+            begin_signal = body.get("begin_signal", "### ")
+            end_signal = body.get("end_signal", "\n")
+            roles_map = body.get(
+                "roles_map", {"user": "Human", "assistant": "Assistant"}
+            )
+            prompt_lines = []
+            if messages[0]["role"] == "system":
+                # the context prompt
+                system_message = messages[0]
+                prompt_lines.append(f"{system_message['content']}\n\n")
+                messages = messages[1:]
+            for message in messages:
+                # remap roles
+                username = roles_map(message["role"])
+                sent = message["content"]
+                input = f"{begin_signal}{username}: {sent}{end_signal}"
+                prompt_lines.append(input)
 
             max_context = body.get("max_context_length", 2048)
 
@@ -49,36 +70,54 @@ class Handler(BaseHTTPRequestHandler):
             ):
                 prompt_lines.pop(0)
 
-            prompt = "\n".join(prompt_lines)
+            prompt = "".join(prompt_lines)
 
-            generator = generate_reply(
-                question=prompt,
-                max_new_tokens=int(body.get("max_length", 200)),
-                do_sample=bool(body.get("do_sample", True)),
-                temperature=float(body.get("temperature", 0.5)),
-                top_p=float(body.get("top_p", 1)),
-                typical_p=float(body.get("typical", 1)),
-                repetition_penalty=float(body.get("rep_pen", 1.1)),
-                encoder_repetition_penalty=1,
-                top_k=int(body.get("top_k", 0)),
-                min_length=int(body.get("min_length", 0)),
-                no_repeat_ngram_size=int(body.get("no_repeat_ngram_size", 0)),
-                num_beams=int(body.get("num_beams", 1)),
-                penalty_alpha=float(body.get("penalty_alpha", 0)),
-                length_penalty=float(body.get("length_penalty", 1)),
-                early_stopping=bool(body.get("early_stopping", False)),
-                seed=int(body.get("seed", -1)),
-                stopping_strings=body.get("stopping_strings", []),
+            n = body.get("n", 1)
+            choices = []
+            for _ in range(n):
+                generator = generate_reply(
+                    question=prompt,
+                    max_new_tokens=int(body.get("max_length", 100000)),
+                    do_sample=bool(body.get("do_sample", True)),
+                    temperature=float(body.get("temperature", 0.5)),
+                    top_p=float(body.get("top_p", 1)),
+                    typical_p=float(body.get("typical_p", 1)),
+                    # TODO is this presence_penalty, frequency_penalty or something else?
+                    repetition_penalty=float(body.get("repetition_penalty", 1.1)),
+                    encoder_repetition_penalty=1,
+                    top_k=int(body.get("top_k", 0)),
+                    min_length=int(body.get("min_length", 0)),
+                    no_repeat_ngram_size=int(body.get("no_repeat_ngram_size", 0)),
+                    num_beams=int(body.get("num_beams", 1)),
+                    penalty_alpha=float(body.get("penalty_alpha", 0)),
+                    length_penalty=float(body.get("length_penalty", 1)),
+                    early_stopping=bool(body.get("early_stopping", False)),
+                    seed=int(body.get("seed", -1)),
+                    stopping_strings=body.get("stop", []),
+                )
+                answer = ""
+                # the generator streams the result, but we are not interested in a stream
+                for a in generator:
+                    if isinstance(a, str):
+                        answer = a
+                    else:
+                        answer = a[0]
+                choices.append(answer[len(prompt) :])
+
+            response = json.dumps(
+                {
+                    "choices": [
+                        {"index": i, "message": {"role": "assistant", "content": x}}
+                        for i, x in enumerate(choices)
+                    ],
+                    # TODO
+                    "usage": {
+                        "prompt_tokens": 0,
+                        "completion_tokens": 0,
+                        "total_tokens": 0,
+                    },
+                }
             )
-
-            answer = ""
-            for a in generator:
-                if isinstance(a, str):
-                    answer = a
-                else:
-                    answer = a[0]
-
-            response = json.dumps({"results": [{"text": answer[len(prompt) :]}]})
             self.wfile.write(response.encode("utf-8"))
         else:
             self.send_error(404)
